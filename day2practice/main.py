07493ac343b3fe68fc1865cd42bb7219d7cd67be
EGORDYU <egordyuzhev@gmail.com>
2023-11-25 11:08:02 -0700

    heh

--- day2practice/main.py
+++ day2practice/main.py
@@ -1,2 +1,7 @@
-print("Hello"[-1])
-print(254+2_525)
+# print("Hello"[-1])
+# print(254+2_525)
+
+
+num_char = len(input("What is your name?"))
+print("Your name has " + num_char + " characters.")
+
