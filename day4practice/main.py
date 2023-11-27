dccdb04f88a5d8f4ec45cf5302b9d5e17aa71167
EGORDYU <egordyuzhev@gmail.com>
2023-11-27 14:04:33 -0700

    day4

---
+++ day4practice/main.py
@@ -0,0 +1,24 @@
+# import random
+# import my_module
+
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+
+# print(random.random())
+
+# random_float = random.random() * 5
+
+
+# love_score = random.randint(1,100)
+# print(f"Your love score is {love_score}")
+
+
+fruits = ["Cherry","Apple","Pear"]
+
+fruits[2] = "Pickle Johnson"
+fruits.append("YEEHAYWWWW")
+
+print(fruits)
+print(fruits[1])
