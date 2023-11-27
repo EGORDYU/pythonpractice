bc613595846f79bc91aed49860f9c92c8b27188b
EGORDYU <egordyuzhev@gmail.com>
2023-11-27 15:35:06 -0700

    starting day4 project

---
+++ day4practice/day4project.py
@@ -0,0 +1,1 @@
+input("What do you choose? Type 0 for ROck, 1 for Paper or 2 for Scissors.\n")
--- day4practice/main.py
+++ day4practice/main.py
@@ -19,6 +19,47 @@ fruits = ["Cherry","Apple","Pear"]
 
 fruits[2] = "Pickle Johnson"
 fruits.append("YEEHAYWWWW")
+fruits.extend(["Banana","Kindred"])
 
 print(fruits)
 print(fruits[1])
+
+starcraft_units = [["zealot"],["zergling"],["marine"]]
+
+
+print(starcraft_units)
+
+
+
+
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+
+coordinates = list(position)
+
+letter = coordinates[0]
+number = int(coordinates[1])
+
+if letter == "A":
+  letter = 0
+elif letter == "B":
+  letter = 1
+elif letter == "C":
+  letter = 2
+  
+
+
+
+
+map[number-1][letter] = "X"
+
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
