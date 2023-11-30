bed3ab7ace4dddccc71798d31f9ea5b9f09f34c1
EGORDYU <egordyuzhev@gmail.com>
2023-11-29 22:50:33 -0700

    random password generator complete

---
+++ day5practice/day5project.py
@@ -0,0 +1,49 @@
+import random
+import string
+
+
+
+
+
+all_letters = list(string.ascii_letters)  # This includes 'a' to 'z' and 'A' to 'Z'
+all_numbers = list(string.digits)  # This includes '0' to '9'
+common_symbols = list(string.punctuation)  # This includes symbols like '@', '#', '$', etc.
+
+print("Welcome to my PyPassword Generator!")
+letters_input = input("How many letters would you like in your password?")
+letters = int(letters_input)  # GETS ME A NUMBER 
+
+symbols_input = input("How many symbols would you like?")
+symbols = int(symbols_input) # GETS ME A NUMBER 
+
+numbers_input = input("How many numbers would you like?")
+numbers = int(numbers_input) # GETS ME A NUMBER 
+
+password = ""
+
+
+# for i in all_letters[letters] :
+
+#     password += str(i)
+
+for i in range(letters):
+    random_number = random.randint(0,len(all_letters)-1)
+    password += all_letters[random_number]
+
+
+for i in range(numbers):
+    random_number = random.randint(0,len(all_numbers)-1)
+    password += all_numbers[random_number]
+
+for i in range(symbols) :
+    random_number = random.randint(0,len(common_symbols)-1)
+    password += common_symbols[random_number]
+
+password = list(password)
+random.shuffle(password)
+print(''.join(password))
+
+
+
+
+
---
+++ day5practice/main.py
@@ -0,0 +1,34 @@
+# champs = ["Diana", "Gwen", "Briar"]
+# egor_champ = ["Kindred", "Gragas"]
+
+
+# for champ in champs:
+#     print(champ + " CRINGE")
+# for champ in egor_champ:
+#     print(champ + " POGGERS")
+
+
+# # Input a list of student scores
+# student_scores = input().split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+
+# # Write your code below this row 👇
+
+# highest_score = student_scores[0]
+# for score in student_scores:
+#   if score > highest_score:
+#       highest_score = score
+    
+
+# print(f"The highest score in the class is: {highest_score}")
+
+# for number in range (a,b,step):
+#     print(number)
+
+add = 0
+
+for number in range (1,101):
+
+    add += number
+    print(add)
