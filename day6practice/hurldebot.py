cd157dab0fe1d7d49556a6d52022c3c5fec91a70
EGORDYU <egordyuzhev@gmail.com>
2023-12-03 12:08:31 -0700

    hurdle challenges done

---
+++ day6practice/hurldebot.py
@@ -0,0 +1,101 @@
+# HURDLE 1
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
+
+# HURDLE 2
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+
+
+# while not at_goal():
+#     jump()
+
+# HURDLE 3
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+
+
+# while not at_goal():
+#    if not wall_in_front():
+#     move()
+#    else:
+#     jump()
+
+# HURDLE 4
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     if not wall_in_front():
+#         move()
+#     else:
+#         jump()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+
+
+# while not at_goal():
+#    if not wall_in_front():
+#      move()
+#    else:
+#      jump()
+       
---
+++ day6practice/main.py
@@ -0,0 +1,13 @@
+
+def my_function():
+    print("Hello")
+    print("This is all from inside the function")
+    input("Name please")
+    print("Hello")
+
+my_function()
+my_function()
+
+
+# while something_is_true:
+    #ddo something repeatadly
