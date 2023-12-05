5a89736dee3d8423d119d678bac001e0e6718aa2
EGORDYU <egordyuzhev@gmail.com>
2023-12-05 14:09:41 -0700

    continuing day8

---
+++ day8practice/main.py
@@ -0,0 +1,20 @@
+name = "Kristina"
+
+
+def say_name(name):
+    print(f"Hi my name is {name}")
+
+
+say_name(name)
+say_name("Egor")
+
+
+
+def greet_with(name, location):
+    print(f"Hello {name} from {location}")
+
+
+kmn = "Kristina"
+kml = "Armenia"
+
+greet_with(kmn,kml)
